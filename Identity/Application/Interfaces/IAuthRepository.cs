5d729ee65feabd8351e416fda4f9024d81620984
AzamFCS <cherkesov.azamatik@gmail.com>
2026-02-01 17:09:41 +0300

    Add project with Docker and migrations

---
+++ Identity/Application/Interfaces/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Entities;
+
+namespace Identity.Application.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<User?> GetByLoginAsync(string login);
+
+        Task<bool> IsLoginTakenAsync(string login);
+
+        Task AddUserAsync(User user);
+
+        Task SaveChangesAsync();
+    }
+}
