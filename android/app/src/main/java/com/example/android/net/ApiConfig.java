68bd3cac49ea7b7797559f648977a16daf84661f
AndreyBry <andrey062006@bk.ru>
2026-05-03 14:27:35 +0300

    Fix errors with port mapping

--- android/app/src/main/java/com/example/android/net/ApiConfig.java
+++ android/app/src/main/java/com/example/android/net/ApiConfig.java
@@ -4,7 +4,7 @@ public final class ApiConfig {
 
     private ApiConfig() {}
 
-    public static final String BASE_URL = "http://10.0.2.2:5283/";
+    public static final String BASE_URL = "http://10.0.2.2:5000/";
 
     public static final String HUB_URL = BASE_URL + "hubs/session";
 }
